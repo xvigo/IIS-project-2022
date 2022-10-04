a30b5298cb1c99663732d8b54fa804015d711dd6
Vilem Gottwald <xgottw07@vutbr.cz>
2022-10-04 23:34:53 +0200

    temporarly revoe oop

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from distutils.log import debug
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 import os
@@ -21,13 +22,13 @@ posts = [
     }
 ]
 
-def create_app(test_config=None):
+#def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='b76d4859dd2d340dfe47dbf8993f0386',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+# app = Flask(__name__, instance_relative_config=True)
+# app.config.from_mapping(
+#     SECRET_KEY='b76d4859dd2d340dfe47dbf8993f0386',
+#     #DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+# )
 
     # if test_config is None:
     #     # load the instance config, if it exists, when not testing
@@ -42,27 +43,35 @@ def create_app(test_config=None):
     # except OSError:
     #     pass
 
-    @app.route("/")
-    @app.route("/home")
-    def home():
-        return render_template('home.html', posts=posts)
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(SECRET_KEY='b76d4859dd2d340dfe47dbf8993f0386')
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
 
 
-    @app.route("/about")
-    def about():
-        return render_template('about.html', title='About')
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
 
-    @app.route("/register", methods=['GET', 'POST'])
-    def register():
-        form = RegistrationForm()
-        if form.validate_on_submit():
-            flash(f'Account created for {form.email.data}', 'success')
-            return redirect(url_for('home'))
-            
-        return render_template('register.html', title='Register', form=form)
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.email.data}', 'success')
+        return redirect(url_for('home'))
+        
+    return render_template('register.html', title='Register', form=form)
 
-    @app.route("/login")
-    def login():
-        form = LoginForm()
-        return render_template('login.html', title='Log In', form=form)
-    return app
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Log In', form=form)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
