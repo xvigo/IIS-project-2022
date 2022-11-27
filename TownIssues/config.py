38a8f1ae0479a45e7b4a0dbd496a2567ca6fbd8a
Vilem Gottwald <xgottw07@vutbr.cz>
2022-11-27 12:55:36 +0100

    finito1

--- TownIssues/config.py
+++ TownIssues/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql', 1)
+    SECRET_KEY = os.urandom(12).hex()
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_CONN_STR')
